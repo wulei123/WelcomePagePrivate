a94d24602b9552ad0a46ffea217b1c7fcab251f5
wulei123 <1534563365@qq.com>
2016-07-12 22:42:31 +0800

    webapp版迎新页 响应式基础布局完成 首页弹出菜单完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,26 @@
 //console.log('\'Allo \'Allo!');
 $(document).ready(function () {
 
+
+
+  $('.header-moblie').on('click',function () {
+    $('.blurBG').css('display','block');
+    $('.header').addClass('header-active');
+    setTimeout(function () {
+      $('.blurBG').addClass('blurBG-active');
+    },2);
+
+
+  })
+
+
+
+
+$('.blurBG').on('click',function () {
+  $('.blurBG').removeClass('blurBG-active');
+  setTimeout(function () {
+    $('.blurBG').css('display','none');
+  },400);
+  $('.header').removeClass('header-active');
+})
 })
